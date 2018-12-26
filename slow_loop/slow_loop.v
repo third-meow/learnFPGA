527543d98807da549f7ded91b4577f42845e4cd6
The third meow <jsheaandcats@gmail.com>
2018-12-26 22:13:55 +1300

    add slow_loop program

---
+++ slow_loop/slow_loop.v
@@ -0,0 +1,21 @@
+module slow_loopl (
+    input CLK,
+    output LED,
+);
+
+    reg [23:0] count;
+    reg led_state;
+
+    always @(posedge CLK) begin
+        count <= count + 1;
+
+        if (count == 16000000) begin    //sub-loop running once per second
+            count <= 0;
+
+            //run once a second:
+            led_state = !led_state;
+        end
+    end
+
+    assign LED = led_state;
+endmodule
