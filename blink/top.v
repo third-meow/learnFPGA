119922ee2879bba7ebb7090cba546b235e7c5635
The third meow <jsheaandcats@gmail.com>
2018-12-26 14:11:45 +1300

    Auto stash before merge of "master" and "origin/master"

---
+++ blink/top.v
@@ -0,0 +1,25 @@
+module blink (
+    input CLK,    // 16MHz clock
+    output LED,   // User LED
+    output USBPU  // USB pull-up resistor
+);
+    // drive USB pull-up resistor to '0' to disable USB
+    assign USBPU = 0;
+
+    // blink circuit:
+
+    // register driver
+    reg [25:0] blink_counter;
+
+    // wire driver
+    // led pattern
+    wire [21:0] blink_pattern = 22'b00000011110000101;
+
+    always @(posedge CLK) begin
+        // increment blink_counter
+        blink_counter <= blink_counter + 1;
+    end
+    
+    // light up the LED according to the pattern
+    assign LED = blink_pattern[blink_counter[25:21]];
+endmodule
