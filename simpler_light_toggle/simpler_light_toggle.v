37444bdae98d65e57edeecc4e92b09430c8ebdbd
The third meow <jsheaandcats@gmail.com>
2018-12-26 20:47:10 +1300

    simplify simpler light-toggle

--- simpler_light_toggle/simpler_light_toggle.v
+++ simpler_light_toggle/simpler_light_toggle.v
@@ -1,17 +1,7 @@
 module simpler_light_toggle (
-	input CLK,   // 16MHz clock
 	input PIN_1,  // pin one
 	output LED,  // onboard led
-	output USBPU // usb pullup resistor
 );
 
-    //register to store led state (eg 0 or 1 {0 = off, 1 = on})
-    reg led_state;
-
-    //runs every clock cycle
-    always @(posedge PIN_1) led_state = !led_state;
-
-    //send led the led_state register
-    assign LED = led_state;
-
+    always @(posedge PIN_1) LED = !LED;
 endmodule
