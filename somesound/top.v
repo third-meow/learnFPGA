c89dca94f7433f6638caadbb175e5c56de863471
The third meow <jsheaandcats@gmail.com>
2019-01-16 13:21:47 +1300

    make sound test work

--- somesound/top.v
+++ somesound/top.v
@@ -1,23 +1,27 @@
 module top(
     input CLK,
-    output PIN_1,
-    output PIN_2
+    input PIN_2,
+    output PIN_1
 );
     
     reg [23:0] count;
+    reg [23:0] freq = 440;
     reg pin_state;
+
 
     always @(posedge CLK) begin
         count <= count + 1;
-        if (count == 160000) begin    //slower sub-loop
+
+        if (PIN_2) begin
+            freq <= ((freq + 1) % 16000);
+        end
+
+        if (count == (16000000/freq)) begin    //slower sub-loop
             count <= 0;
-
             pin_state = !pin_state;
-
         end
     end
 
     assign PIN_1 = pin_state;
-    assign PIN_2 = !pin_state;
 endmodule
 
